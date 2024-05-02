454e379dc79ff0ddf5b91a7893f92c90a6de984d
tdokhelar <84328717+tdokhelar@users.noreply.github.com>
2024-05-02 15:58:37 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  var map = L.map('map').setView([48.8566, 2.3522], 13); // Paris as center
-  var gogocarto = new GogoCarto({
-    map: map,
-    gogocartoUrl: 'https://gogocarto.fr',
-    layers: ['services', 'contributors']
-  });
-  gogocarto.loadLayers();
-});
