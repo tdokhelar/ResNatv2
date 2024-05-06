930271bf79e4bce67447a3c482a97fe052783604
tdokhelar <84328717+tdokhelar@users.noreply.github.com>
2024-05-06 22:12:59 +0200

    Delete asset directory

--- asset/js/admin/form-fields/mongo-association-field.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  // Adds a class to the mongo association fields so we can
-  // easily change the style
-
-  // small hack to find those kind of components
-  $('.field-actions > .btn-group + .modal').each(function() {
-    $(this).closest('.sonata-ba-field').addClass('mongo-association-field')
-  })
-})
--- asset/js/utils.js
+++
@@ -1,6 +0,0 @@
-export function isImageUrl(value) {
-  if (!value || typeof value != 'string') return false
-  let imagePattern = new RegExp('.*\.(svg|png|jpg|jpeg|gif)')
-  let urlPattern = new RegExp('https?:\/\/')
-  return imagePattern.test(value.toLowerCase()) || urlPattern.test(value.toLowerCase());
-}
